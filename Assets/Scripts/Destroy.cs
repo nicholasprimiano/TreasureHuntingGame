08c519a1d3c4dae8561da1059711a5115b355d45
Nicholas Primiano <njp280@nyu.edu>
2016-07-19 18:02:36 -0400

    no message

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour
+{
+	
+	public int damage = 1;
+
+	//	void OnCollisionEnter2D (Collision coll){
+	//		if (coll.gameObject.tag == "Enemy") {
+	//			coll.GetComponent < Killable>().Hurt
+	//		}
+	//	}
+	//
+
+
+
+	// A function that is automatically called when
+	// as long as a Rigidibody2D is in the tirgger, each fame
+	void OnTriggerStay2D (Collider2D activator)
+	{
+		// Does the activating thing have a killable script on it
+		if (activator.GetComponent<Killable> () != null) {          
+			activator.GetComponent<Killable> ().Hurt (damage);
+			// Destroy this object
+			// Destroy (activator.gameObject);
+		}    
+	}
+		
+
+
+}
+
---
+++ Assets/Scripts/Killable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killable : MonoBehaviour
+{
+	public int maxHealth = 100;
+	public int currentHealth = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// everyone starts with 100% health at the beginning of the game
+		currentHealth = maxHealth;
+	}
+
+	//Public so death trigger can talk to it
+	public void Hurt (int damage)
+	{
+		currentHealth -= damage;
+		currentHealth = Mathf.Clamp (currentHealth, 0, maxHealth);
+
+		if (currentHealth <= 0) {
+			Destroy (gameObject);
+		}
+
+		Debug.Log (currentHealth);
+	}
+}
