2dfead8ec1c4f9f98b2b0ca1415ba31a0b3e2776
Nicholas Primiano <njp280@nyu.edu>
2016-07-21 04:05:28 -0400

    no message

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -20,10 +20,10 @@ public class Treasure : MonoBehaviour
 	{
 		textbuffer.text = "Treasure is waiting for you somewhere";
 
-		if ((PlayerObject.position - Hint1.position).magnitude < 20f) {
-			textbuffer.text = "Sorry, not the right place.";
+		if ((PlayerObject.position - Hint1.position).magnitude < 25f) {
+			textbuffer.text = "SHOOT!!!";
 		} else if ((PlayerObject.position - Hint2.position).magnitude < 20f) {
-			textbuffer.text = "Haha you are fooled.";
+			textbuffer.text = "Try another way!";
 		} else if ((PlayerObject.position - Hint3.position).magnitude < 20f) {
 			textbuffer.text = "I am sure you can get there if you choose a different path....";
 		} else if ((PlayerObject.position - Hint4.position).magnitude < 20f) {
