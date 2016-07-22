b7fe27e3c2421c8ff58db7aee5be5bcef5bbe52f
vincent primiano <vincent primiano>
2016-07-22 09:58:30 -0400

    no message

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,20 +3,30 @@ using System.Collections;
 
 public class Timer : MonoBehaviour {
 
-	float timer;
 
-	void update(){
-		timer = Time.time;
+
+
+
+
+
+	private float timer = 0;
+	void Update(){
 		timer += Time.deltaTime;
 	}
 
-	void OnGUI() {
-		int minutes = Mathf.FloorToInt(timer / 60F);
-		int seconds = Mathf.FloorToInt(timer - minutes * 60);
+	private GUIStyle guiStyle = new GUIStyle();
 
-	    string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
-
-		GUI.Label(new Rect(10,10,250,100), niceTime);
+	void OnGUI(){
+		int minutes = Mathf.FloorToInt (timer / 60f);
+		int seconds = Mathf.FloorToInt (timer - minutes * 60);
+		string time = string.Format ("{0:00}:{1:00}", minutes, seconds);
+		GUI.color = Color.black;
+		guiStyle.fontSize = 20;
+		GUI.Label (new Rect (10, 10, 500, 200), "Game Timer :", guiStyle);
+		GUI.Label (new Rect (136, 10, 500, 200), time, guiStyle);
 	}
-		
+
+
+
+			
 }
