086d7be64fd1b78413155c089c1cd4028ec765dd
Nicholas Primiano <njp280@nyu.edu>
2016-07-23 15:03:45 -0400

    no message

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+
+	static PlayerName instance = null;
+	public Text playerName;
+	public string nameReferance;
+	public GameObject startButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+		startButton.SetActive (false);
+	}
+
+
+	public void getText ()
+	{
+		nameReferance = playerName.text;
+		Debug.Log (nameReferance);
+		startButton.SetActive (true);
+	}
+
+}
---
+++ Assets/Scripts/ShowPlayerName.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowPlayerName : MonoBehaviour
+{
+	public Text playerNameUIText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PlayerName playerName = FindObjectOfType<PlayerName> ();
+		playerNameUIText.text = playerName.nameReferance;
+	}
+}
