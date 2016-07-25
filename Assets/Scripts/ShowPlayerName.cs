b3c7221e9953f0013de052641099e4c0908aa828
Nicholas Primiano <njp280@nyu.edu>
2016-07-25 04:13:19 -0400

    no message

--- Assets/Scripts/ShowPlayerName.cs
+++ Assets/Scripts/ShowPlayerName.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 public class ShowPlayerName : MonoBehaviour
 {
 	public Text playerNameUIText;
+	public PlayerName temp;
 
 	// Use this for initialization
 	void Start ()
 	{
 		PlayerName playerName = FindObjectOfType<PlayerName> ();
+		if (playerName == null) {
+			playerName = Instantiate (temp);
+		}
 		playerNameUIText.text = playerName.nameReferance;
 	}
 }
